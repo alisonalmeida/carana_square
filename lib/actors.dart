94e5adac6fa980422b1b7a3670c19d140e0c0f3e
AlisonAlmeida <alison.asa@gmail.com>
2023-01-17 15:00:21 -0400

    commit

--- lib/actors.dart
+++ lib/actors.dart
@@ -2,14 +2,10 @@ import 'package:carana_square/carana_square.dart';
 import 'package:carana_square/consts.dart';
 import 'package:flame/components.dart';
 
-class Player1 extends SpriteAnimationComponent with HasGameRef<CaranaGame> {
-  Player1({
+class Player extends SpriteAnimationComponent with HasGameRef<CaranaGame> {
+  Player({
     required super.position,
   }) : super(size: Vector2.all(64), anchor: Anchor.center);
-
-  void move(Vector2 delta) {
-    position.add(delta);
-  }
 
   @override
   Future<void> onLoad() async {
@@ -17,11 +13,55 @@ class Player1 extends SpriteAnimationComponent with HasGameRef<CaranaGame> {
       game.images.fromCache(playerHeloizaPath),
       SpriteAnimationData.sequenced(
         amountPerRow: 4,
-        amount: 32,
+        amount: 4, //max32
         stepTime: 0.2,
         textureSize: Vector2.all(32),
         texturePosition: Vector2.all(4),
       ),
     );
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    final bool moveLeft = gameRef.joystick.direction == JoystickDirection.left;
+    final bool moveRight =
+        gameRef.joystick.direction == JoystickDirection.right;
+    final bool moveUp = gameRef.joystick.direction == JoystickDirection.up;
+    final bool moveDown = gameRef.joystick.direction == JoystickDirection.down;
+    final bool moveDownLeft = gameRef.joystick.direction == JoystickDirection.downLeft;
+    final bool moveUpLeft =
+        gameRef.joystick.direction == JoystickDirection.upLeft;
+        final bool moveDownRight =
+        gameRef.joystick.direction == JoystickDirection.downRight;
+    final bool moveUpRight =
+        gameRef.joystick.direction == JoystickDirection.upRight;
+
+
+    final double playerVectorX =
+        (gameRef.joystick.relativeDelta * 300 * dt)[0];
+    final double playerVectorY =
+        (gameRef.joystick.relativeDelta * 300 * dt)[1];
+
+
+    //  is moving left
+    if (moveLeft && x > 0) {
+      x += playerVectorX;
+    }
+//  is moving right
+    if (moveRight && x < gameRef.size[0]) {
+      x += playerVectorX;
+    }
+
+    // s moving up
+    if (moveUp && y > 0) {
+      y += playerVectorY;
+    }
+
+    //  moving down
+    if (moveDown && y < gameRef.size[1] - height) {
+      y += playerVectorY;
+    }
+    
   }
 }
--- lib/carana_square.dart
+++ lib/carana_square.dart
@@ -8,7 +8,8 @@ import 'package:flutter/material.dart';
 
 class CaranaGame extends FlameGame with HasDraggables {
   CaranaGame();
-  late Player1 _player;
+  late Player _player;
+  late JoystickComponent joystick;
 
   @override
   Future<void> onLoad() async {
@@ -16,14 +17,16 @@ class CaranaGame extends FlameGame with HasDraggables {
     final backgroundPaint = BasicPalette.gray.withAlpha(80).paint();
 
     await Flame.images.loadAll([tileMapPath, playerHeloizaPath]);
-    _player = Player1(position: Vector2.all(32));
+    _player = Player(position: Vector2.all(32));
+    
     add(_player);
 
-    final joystick = JoystickComponent(
+    joystick = JoystickComponent(
       knob: CircleComponent(radius: 20, paint: knobPaint),
       background: CircleComponent(radius: 50, paint: backgroundPaint),
       margin: const EdgeInsets.only(left: 40, bottom: 40),
     );
+  
     add(joystick);
   }
 }
