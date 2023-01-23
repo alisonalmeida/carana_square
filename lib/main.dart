27d64b796dd1d6e5990c4c6fc22559d34a640195
AlisonAlmeida <alison.asa@gmail.com>
2023-01-23 13:55:19 -0400

    changed SystemUiMode

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:carana_square/carana_square.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   await Flame.device.setLandscape();
-  
+
   runApp(
     const GameWidget<CaranaGame>.controlled(gameFactory: CaranaGame.new),
   );
